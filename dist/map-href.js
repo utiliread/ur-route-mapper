3fdd5cfa3027d75a8a513331a64c2256fa155c74
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2017-11-02 17:23:11 +0100

    chore(tsconfic) include decorator metadata

--- dist/map-href.js
+++ dist/map-href.js
@@ -4,6 +4,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 import { bindable, customAttribute, inject } from 'aurelia-framework';
 import { RouteMapper } from './route-mapper';
 var MapperHref = /** @class */ (function () {
@@ -25,17 +28,21 @@ var MapperHref = /** @class */ (function () {
         }
     };
     __decorate([
-        bindable({ changeHandler: 'processChange', primaryProperty: true })
+        bindable({ changeHandler: 'processChange', primaryProperty: true }),
+        __metadata("design:type", String)
     ], MapperHref.prototype, "route", void 0);
     __decorate([
-        bindable({ changeHandler: 'processChange' })
+        bindable({ changeHandler: 'processChange' }),
+        __metadata("design:type", Object)
     ], MapperHref.prototype, "params", void 0);
     __decorate([
-        bindable()
+        bindable(),
+        __metadata("design:type", Object)
     ], MapperHref.prototype, "attribute", void 0);
     MapperHref = __decorate([
         inject(Element, RouteMapper),
-        customAttribute('map-href')
+        customAttribute('map-href'),
+        __metadata("design:paramtypes", [Element, RouteMapper])
     ], MapperHref);
     return MapperHref;
 }());
