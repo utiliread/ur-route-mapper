fbff0238414d123273731870c916fceddc6b0947
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2018-01-09 11:30:29 +0100

    chore(tsconfig) target esnext

--- dist/mapper-href.js
+++ dist/mapper-href.js
@@ -9,24 +9,24 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 import { TaskQueue, autoinject, bindable, customAttribute } from 'aurelia-framework';
 import { RouteMapper } from './route-mapper';
-var MapperHref = /** @class */ (function () {
-    function MapperHref(element, mapper, taskQueue) {
+let MapperHref = class MapperHref {
+    constructor(element, mapper, taskQueue) {
         this.element = element;
         this.mapper = mapper;
         this.taskQueue = taskQueue;
         this.attribute = 'href';
         this.pendingChanges = false;
     }
-    MapperHref.prototype.processChange = function () {
+    processChange() {
         // Delay the updating until after both route and params are set
         this.pendingChanges = true;
         this.taskQueue.queueTask(this.makeHref.bind(this));
-    };
-    MapperHref.prototype.makeHref = function () {
+    }
+    makeHref() {
         if (this.pendingChanges) {
             if (this.route) {
-                var href = this.mapper.generate(this.route, this.params);
-                var element = this.element;
+                let href = this.mapper.generate(this.route, this.params);
+                let element = this.element;
                 if (element.au.controller) {
                     element.au.controller.viewModel[this.attribute] = href;
                 }
@@ -36,24 +36,23 @@ var MapperHref = /** @class */ (function () {
             }
             this.pendingChanges = false;
         }
-    };
-    __decorate([
-        bindable({ changeHandler: 'processChange', primaryProperty: true }),
-        __metadata("design:type", String)
-    ], MapperHref.prototype, "route", void 0);
-    __decorate([
-        bindable({ changeHandler: 'processChange' }),
-        __metadata("design:type", Object)
-    ], MapperHref.prototype, "params", void 0);
-    __decorate([
-        bindable(),
-        __metadata("design:type", Object)
-    ], MapperHref.prototype, "attribute", void 0);
-    MapperHref = __decorate([
-        autoinject(),
-        customAttribute('mapper-href'),
-        __metadata("design:paramtypes", [Element, RouteMapper, TaskQueue])
-    ], MapperHref);
-    return MapperHref;
-}());
+    }
+};
+__decorate([
+    bindable({ changeHandler: 'processChange', primaryProperty: true }),
+    __metadata("design:type", String)
+], MapperHref.prototype, "route", void 0);
+__decorate([
+    bindable({ changeHandler: 'processChange' }),
+    __metadata("design:type", Object)
+], MapperHref.prototype, "params", void 0);
+__decorate([
+    bindable(),
+    __metadata("design:type", Object)
+], MapperHref.prototype, "attribute", void 0);
+MapperHref = __decorate([
+    autoinject(),
+    customAttribute('mapper-href'),
+    __metadata("design:paramtypes", [Element, RouteMapper, TaskQueue])
+], MapperHref);
 export { MapperHref };
--- dist/route-mapper.js
+++ dist/route-mapper.js
@@ -1,38 +1,19 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 import { RouteRecognizer } from 'aurelia-route-recognizer';
-var RouteMapper = /** @class */ (function (_super) {
-    __extends(RouteMapper, _super);
-    function RouteMapper() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    RouteMapper.prototype.map = function (routes, parentRoute, parentName) {
-        var _this = this;
-        if (parentRoute === void 0) { parentRoute = ''; }
-        if (parentName === void 0) { parentName = ''; }
-        routes.forEach(function (config) {
-            var name = parentName
-                ? parentName + "." + config.name
+export class RouteMapper extends RouteRecognizer {
+    map(routes, parentRoute = '', parentName = '') {
+        routes.forEach(config => {
+            let name = parentName
+                ? `${parentName}.${config.name}`
                 : config.name;
-            var path = parentRoute + "/" + config.route;
-            _this.add({
+            let path = `${parentRoute}/${config.route}`;
+            this.add({
                 path: path,
                 handler: { name: name },
                 caseSensitive: config.caseSensitive === true
             });
             if (config.settings && config.settings.childRoutes) {
-                _this.map(config.settings.childRoutes, path, name);
+                this.map(config.settings.childRoutes, path, name);
             }
         });
-    };
-    return RouteMapper;
-}(RouteRecognizer));
-export { RouteMapper };
+    }
+}
